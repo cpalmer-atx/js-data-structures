dc14796fb6150bd17634b5bcdfa13b05fbc2d94c
Chad Palmer <chad.n.palmer@gmail.com>
2022-04-21 14:30:00 -0500

    begin adding class skeletons

--- src/challenges.js
+++ src/challenges.js
@@ -1,1 +1,66 @@
 const sanityCheck = () => { return 1; }
+
+
+// ################# BEGIN STACK ################# //
+
+const Stack = function() {
+    // Define stack class here
+};
+
+Stack.prototype.push = function() {
+    // stack push method
+};
+
+Stack.prototype.pop = function() {
+    // stack push method
+};
+
+Stack.prototype.peek = function() {
+    // stack push method
+};
+
+
+// ################# BEGIN QUEUE ################# //
+
+const Queue = function() {
+    // Define queue class here
+};
+
+Queue.prototype.add = function() {
+    // queue add method
+};
+
+Queue.prototype.remove = function() {
+    // queue remove method
+};
+
+Queue.prototype.peek = function() {
+    // queue peek method
+};
+
+
+// ############### BEGIN LINKEDLIST ############## //
+
+const LinkedList = function() {
+    // Define linked list class here
+};
+
+LinkedList.prototype.addToHead = function() {
+    // linked list addToHead method
+};
+
+LinkedList.prototype.addToTail = function() {
+    // linked list addToTail method
+};
+
+LinkedList.prototype.deleteHead = function() {
+    // linked list deleteHead method
+};
+
+LinkedList.prototype.deleteTail = function() {
+    // linked list deleteTail method
+};
+
+LinkedList.prototype.find = function() {
+    // linked list find method
+};
